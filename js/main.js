4e04a3d26168a455f8fa5d5fa86da940f9b38ea7
Vyacheslav Golovanov <privetslavka@gmail.com>
2021-01-31 01:47:16 +0300

    Правки сравнений

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const isNumberPositive = (num) => (num >= 0);
-
 // Случайное число из диапазона (min и max включительно)
 
 const getRandomNumber = (min, max) => {
-  if (isNumberPositive(min) && isNumberPositive(max) && max > min) {
+  if (min >= 0 && min < max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
@@ -15,7 +13,7 @@ const getRandomNumber = (min, max) => {
 // Случайное число c плавающей точкой из диапазона (min и max включительно)
 
 const getRandomFloat = (min, max, digits) => {
-  if (isNumberPositive(min) && isNumberPositive(max) && max > min) {
+  if (min >= 0 && min < max) {
     return +(Math.random() * (max - min + 1) + min).toFixed(digits);
   }
 
