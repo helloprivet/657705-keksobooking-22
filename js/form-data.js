a5a53a00cb560b32d1e2fc3d7518ef07ddcb6248
Vyacheslav Golovanov <privetslavka@gmail.com>
2021-02-25 10:10:12 +0300

    Помощь друга (часть 2)

--- js/form-data.js
+++ js/form-data.js
@@ -2,6 +2,7 @@
 
 import {map} from './page-init.js';
 import {createMarker} from './map.js';
+import {validation} from './validation.js';
 
 const mainPinIcon = L.icon({
   iconUrl: '../img/main-pin.svg',
@@ -16,8 +17,16 @@ const typeMinPrice = {
   palace: 10000,
 };
 
-const mainPin = createMarker(35.652832, 139.839478, true, mainPinIcon);
-mainPin.addTo(map);
+const titleValidRules = {
+  minLenght: 30,
+  maxLenght: 100,
+  required: true,
+};
+
+const priceValidRules = {
+  required: true,
+  maxValue: 1000000,
+};
 
 const userForm = document.querySelector('.ad-form');
 const userFormType = userForm.querySelector('#type');
@@ -25,13 +34,21 @@ const userFormPrice = userForm.querySelector('#price');
 const userFormTimein = userForm.querySelector('#timein');
 const userFormTimeout = userForm.querySelector('#timeout');
 const userFormAddress = userForm.querySelector('#address');
+const userFormTitle = userForm.querySelector('#title');
+
+const userFormRoomNumber = userForm.querySelector('#room_number');
+
+const userFormCapacity = userForm.querySelector('#capacity');
+const userFormCapacityOption = userFormCapacity.querySelectorAll('option');
+
+const mainPin = createMarker(35.65283, 139.83947, true, mainPinIcon).addTo(map);
 
 const changeAddress = (evt) => {
   const {lat, lng} = evt.target.getLatLng();
-  userFormAddress.value = `${lat.toFixed(6)}, ${lng.toFixed(6)}`;
+  userFormAddress.value = `${lat.toFixed(5)}, ${lng.toFixed(5)}`;
 };
 
-const changeType = (evt) => {
+const changeTypePrice = (evt) => {
   userFormPrice.placeholder = typeMinPrice[evt.target.value];
   userFormPrice.min = typeMinPrice[evt.target.value];
 };
@@ -47,8 +64,38 @@ const changeTimeInOut = (evt) => {
   }
 };
 
-userFormType.addEventListener('change', changeType);
+const changeCapacityNumber = (evt) => {
+  const value = evt.target.value;
+
+  userFormCapacityOption.forEach((item) => {
+    item.disabled = true;
+  });
+
+  if (value < 100) {
+    for (let i = 1; i <= value; i++) {
+      userFormCapacityOption.forEach((item) => {
+        if (+item.value === i) {
+          item.disabled = false;
+          userFormCapacity.value = i;
+        }
+      });
+    }
+  } else {
+    userFormCapacityOption.forEach((item) => {
+      if (+item.value === 0) {
+        item.disabled = false;
+        userFormCapacity.value = 0;
+      }
+    });
+  }
+};
+
+mainPin.on('moveend', changeAddress);
+userFormType.addEventListener('change', changeTypePrice);
+userFormPrice.addEventListener('focus', validation(priceValidRules));
+userFormPrice.addEventListener('input', validation(priceValidRules));
 userFormTimein.addEventListener('change', changeTimeInOut);
 userFormTimeout.addEventListener('change', changeTimeInOut);
-mainPin.on('moveend', changeAddress);
-
+userFormTitle.addEventListener('focus', validation(titleValidRules));
+userFormTitle.addEventListener('input', validation(titleValidRules));
+userFormRoomNumber.addEventListener('change', changeCapacityNumber);
---
+++ js/validation.js
@@ -0,0 +1,116 @@
+'use strict';
+
+const validationTextErrors = {
+  required: () => {
+    return 'Обязательное поле. Пожалуйста, заполните его. CUSTOM';
+  },
+
+  minLenght: (evt, obj) => {
+    let {minLenght} = obj;
+    const left = minLenght - evt.target.value.length;
+    return `Минимальная длина строки ${minLenght} символов. Осталось ${left} символов. CUSTOM`;
+  },
+
+  maxLenght: (evt, obj) => {
+    let {maxLenght} = obj;
+    return `Максимальная длина строки ${maxLenght} символов. CUSTOM`;
+  },
+
+  maxValue: (evt, obj) => {
+    let {maxValue} = obj;
+    return `Максимальная цена - ${maxValue} руб. CUSTOM`;
+  },
+};
+
+const validationParameters = (obj) => {
+  if (typeof obj !== 'object' && Object.keys(obj).length === 0) {
+    return false;
+  }
+
+  return obj;
+};
+
+const inputRequired = (evt, obj) => {
+  let validValue = {};
+  for (let key in obj) {
+    switch (key) {
+      case 'required':
+        validValue[key] = evt.target.value.length === 0;
+        break;
+    }
+  }
+
+  return validValue;
+};
+
+const inputLenght = (evt, obj) => {
+  let validValue = {};
+  for (let key in obj) {
+    switch (key) {
+      case 'minLenght':
+        validValue[key] = evt.target.value.length < obj[key];
+        break;
+      case 'maxLenght':
+        validValue[key] = evt.target.value.length > obj[key];
+        break;
+    }
+  }
+
+  return validValue;
+};
+
+const inputValue = (evt, obj) => {
+  let validValue = {};
+
+  for (let key in obj) {
+    switch (key) {
+      case 'maxValue':
+        validValue[key] = evt.target.value >= 1000000;
+        break;
+    }
+  }
+
+  return validValue;
+};
+
+const validationFunctions = {
+  required: inputRequired,
+  minLenght: inputLenght,
+  maxLenght: inputLenght,
+  maxValue: inputValue,
+};
+
+const validation = (obj) => {
+  return (evt) => {
+    let validValue = {};
+    let messages = [];
+    const parameters = validationParameters(obj);
+
+    if (parameters) {
+      for (let key in parameters) {
+        if (key in validationFunctions) {
+          const currentValue = validationFunctions[key](evt, obj);
+          for (let key in currentValue) {
+            validValue[key] = currentValue[key];
+          }
+        }
+      }
+
+      for (let key in validValue) {
+        if (validValue[key]) {
+          messages.push(validationTextErrors[key](evt, obj));
+        }
+      }
+    }
+
+    if (messages.length !== 0) {
+      evt.target.setCustomValidity(messages.join('\n'));
+    } else {
+      evt.target.setCustomValidity('');
+    }
+
+    evt.target.reportValidity();
+  };
+};
+
+export {validation};
